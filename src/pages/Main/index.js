de7ef848e80c6638208f514629f21397841680da
Julio Junior <juniorknx@hotmail.com>
2022-03-30 00:58:50 -0300

    Adicionando lottie e animation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,19 @@
 import { FormGenerator } from '../../Components/FormGenerator';
+import Lottie from 'react-lottie';
 import './styles.scss';
+import wppanimate from '../../animations/whatsappanimate.json'
 
 export function Main(){
     return (
         <>
         <div className="main__container">
             <FormGenerator />
+            <Lottie options={{
+                animationData:wppanimate,
+                autoplay:true,
+                loop:true,
+            }}
+            />
         </div>
         </>
     )
